0ecbab8a638792f2c0441221f219e7428fed7315
yumi19980927 <yumi.jimbo@rakus-partners.co.jp>
2021-11-15 11:45:03 +0900

    Articleクラスを作成

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,22 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    articles: [
+      new Article(3, "佐藤", "佐藤さんの記事", []),
+      new Article(2, "山田", "山田さんの記事", [
+        new Comment(13, "山崎", "山崎さんのコメント", 2),
+      ]),
+      new Article(1, "伊藤", "伊藤さんの記事", [
+        new Comment(12, "鈴木", "鈴木さんのコメント", 1),
+        new Comment(11, "吉田", "吉田さんのコメント", 1),
+      ]),
+    ],
+  }, // end state
+  mutations: {},
+  actions: {},
+  modules: {},
+});
---
+++ src/types/Article.ts
@@ -0,0 +1,40 @@
+export class Article {
+  constructor(
+    private _id: number,
+    private _name: string,
+    private _content: string,
+    private _commentList: Array<Comment>
+  ) {}
+
+  public get id(): number {
+    return this._id;
+  }
+
+  public set id(id: number) {
+    this._id = id;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public set name(name: string) {
+    this._name = name;
+  }
+
+  public get content(): string {
+    return this._content;
+  }
+
+  public set content(content: string) {
+    this._content = content;
+  }
+
+  public get commentList(): Array<Comment> {
+    return this._commentList;
+  }
+
+  public set commentList(commentList: Array<Comment>) {
+    this._commentList = commentList;
+  }
+}
